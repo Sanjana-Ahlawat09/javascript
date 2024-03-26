e20c1d8ffa16aa56828f522c0b0f14957dccd5f8
Sanjana-Ahlawat09 <149235944+Sanjana-Ahlawat09@users.noreply.github.com>
2024-03-26 08:52:24 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,15 @@
+// "use strict"  //treat all JS files as newer version
+// alert(3+3)
+//PRIMITIVE DATATYPES
+// number => 2 to power 53
+// bigInt
+// string=> ""
+// Boolean => true/false
+// null=> standalone value
+//undefined =>
+// symbol =>unique
+
+//OBJECTS
+age=  13;
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
