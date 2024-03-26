5545634837596949929c6ffc107b62ab7ff6c933
Sanjana-Ahlawat09 <149235944+Sanjana-Ahlawat09@users.noreply.github.com>
2024-03-26 09:21:31 +0000

    convertingOperations

---
+++ basics/convertingOperations.js
@@ -0,0 +1,16 @@
+// let score =undefined
+
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1; false => 0
+// "Hitesh" => true
+// ""=>0
+
+let isLogin = 1
+let booleanIsLogin = Boolean(isLogin)
+console.log(booleanIsLogin);
