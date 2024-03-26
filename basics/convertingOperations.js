c5361a820b185ae94a4423790a275fb6a35147e7
Sanjana-Ahlawat09 <149235944+Sanjana-Ahlawat09@users.noreply.github.com>
2024-03-26 09:42:04 +0000

    convertingOperations

--- basics/convertingOperations.js
+++ basics/convertingOperations.js
@@ -11,6 +11,28 @@
 // "Hitesh" => true
 // ""=>0
 
-let isLogin = 1
-let booleanIsLogin = Boolean(isLogin)
-console.log(booleanIsLogin);
+// let isLogin = 1
+// let booleanIsLogin = Boolean(isLogin)
+// console.log(booleanIsLogin);
+
+//**********************Operations****************************
+
+// let value = 3
+// let negValue = -value
+//console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+ let str1 = 33
+ let str2 = " everyone"
+// let str3 = str1*str2
+// console.log(str3);
+let str4 = str1+str2
+console.log(str4)
+
+console.log(typeof str4 );
+console.log(1+2+"4");
+console.log("1"+2+4);
