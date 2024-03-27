86052402f3c10fd4c902acbc11046752bee50a4d
Sanjana-Ahlawat09 <149235944+Sanjana-Ahlawat09@users.noreply.github.com>
2024-03-27 12:57:39 +0000

    stackHeap

---
+++ basics/stackHeap.js
@@ -0,0 +1,15 @@
+//stack(primitive),heap(non-primitive)
+let myYoutubername = "sanjanaahlawatcom"
+let anothername = myYoutubername
+anothername = "chai aur code"
+console.log(myYoutubername);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi:"user@ybl"
+}
+let userTwo = userOne
+userTwo.email ="sanjana@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
