ee9064a0397c035c9a6f3c6fc0344fdf442b9307
Sanjana-Ahlawat09 <149235944+Sanjana-Ahlawat09@users.noreply.github.com>
2024-03-27 10:10:13 +0000

    comaprision01

---
+++ basics/comparision01.js
@@ -0,0 +1,8 @@
+console.log(null>0);//false
+console.log(null == 0);//false
+console.log(null>=0);//true
+console.log(undefined == 0);//false
+console.log(undefined>0);//false
+console.log(undefined<0);//false
+console.log("2"==2);//true bcz it only checks values.
+console.log("2"===2);//false,bcz it also checks datatypes of values which are not same.
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,11 @@
-console.log("sanjana")
+const accountId = 14838
+let accountEmail = "sanjana54@google.com"
+var accountPassword = "Jaipur"
+accountCity="jaipur"
+let accountState
+// accountId = 2
+accountEmail="hc@hc.com"
+accountPassword="59745046"
+accountCity="meerut"
+console.log(accountId)
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
