0a95f3c01747bb24d06c0849cb52789f6042c4d6
Sanjana-Ahlawat09 <149235944+Sanjana-Ahlawat09@users.noreply.github.com>
2024-03-30 02:03:55 +0000

    object2

---
+++ basics2/object2.js
@@ -0,0 +1,11 @@
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"sanjana"
+}
+// course.courseInstructor
+
+const{courseInstructor:instructor}= course//destructuring
+console.log(instructor);
+
+ 
