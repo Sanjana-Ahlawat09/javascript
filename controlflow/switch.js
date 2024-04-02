8062dc667c5c1f4560975aa153e0e70f27af0d99
Sanjana-Ahlawat09 <149235944+Sanjana-Ahlawat09@users.noreply.github.com>
2024-04-02 19:02:23 +0000

    switch

---
+++ controlflow/switch.js
@@ -0,0 +1,15 @@
+const month = "march"
+switch(month){
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    default:
+            console.log("default");
+            break;
+}
